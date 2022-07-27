91357e41f6b1f03c08e6a754698d3f0345792b6e
Ben Hall <ben@benh.codes>
2022-07-27 20:14:37 +0100

    wip(menu): Started to design menu
    Signed-off-by: Ben Hall <ben@benh.codes>

--- internal/pkg/discord/cmds/create_info_menu/create_info_menu.go
+++ internal/pkg/discord/cmds/create_info_menu/create_info_menu.go
@@ -32,20 +32,21 @@ func (c Command) Handle(s *discordgo.Session, i *discordgo.InteractionCreate) {
 			Welcome to Tubbo's Discord! Please use the buttons below to navigate information about the server.
 
 			**Socials**:
-			%[1]s YouTube [@Tubbo](https://www.youtube.com/Tubbo)
-			%[2]s Twitter [@TubboLive](https://twitter.com/TubboLive)
-			%[2]s Twitter Alt [@TubboTWO](https://twitter.com/TubboTWO)
-			%[3]s Twitch [@Tubbo](https://twitch.tv/Tubbo)
-			%[4]s Twitch Alt [@TubboLIVE](https://twitch.tv/TubboLIVE)
-			%[4]s Reddit [@Tubbo](https://www.reddit.com/r/Tubbo)
-			%[5]s Merch [ShopTubbo.com](https://shoptubbo.com)
-			%[6]s Spotify [Tubbo](https://open.spotify.com/user/p04neko25tv0jw7p24si4pcis)
-			%[6]s Spotify (Artist) [Tubbo](https://open.spotify.com/artist/4B1kkhDbhSyiS3VDgbKV2T)
+			%[1]s Instagram [@TubboLive](https://instagram.com/TubboLive)
+			%[2]s Merch [ShopTubbo.com](https://shoptubbo.com)
+			%[3]s Reddit [@Tubbo](https://www.reddit.com/r/Tubbo)
+			%[4]s Spotify [Tubbo](https://open.spotify.com/user/p04neko25tv0jw7p24si4pcis)
+			%[4]s Spotify (Artist) [Tubbo](https://open.spotify.com/artist/4B1kkhDbhSyiS3VDgbKV2T)
+			%[6]s Twitch [@Tubbo](https://twitch.tv/Tubbo)
+			%[6]s Twitch Alt [@TubboLIVE](https://twitch.tv/TubboLIVE)
+			%[5]s Twitter [@TubboLive](https://twitter.com/TubboLive)
+			%[5]s Twitter Alt [@TubboTWO](https://twitter.com/TubboTWO)
+			%[7]s YouTube [@Tubbo](https://www.youtube.com/Tubbo)
 
 			**Contact**:
 			- Email (Business) [tubbobusiness@gmail.com](mailto:tubbobusiness@gmail.com)
 			- Email (Merch) [support@shoptubbo.com](mailto:support@shoptubbo.com)
-		`, utils.Emoji["blurple_youtube"], utils.Emoji["blurple_twitter"], utils.Emoji["blurple_twitch"], utils.Emoji["blurple_reddit"], utils.Emoji["blurple_merch"], utils.Emoji["blurple_spotify"], utils.Emoji["blurple_email"]),
+		`, utils.Emoji["blurple_instagram"], utils.Emoji["blurple_merch"], utils.Emoji["blurple_reddit"], utils.Emoji["blurple_spotify"], utils.Emoji["blurple_twitch"], utils.Emoji["blurple_twitter"], utils.Emoji["blurple_youtube"]),
 	})
 	s.ChannelMessageSendComplex(infid, &discordgo.MessageSend{
 		Embeds: []*discordgo.MessageEmbed{&embed},
