3f405339a741822b57a2f3c6c9583b99d1dbfaa5
Ben Hall <ben@benh.codes>
2022-08-06 12:19:08 +0100

    feat: Events
    Signed-off-by: Ben Hall <ben@benh.codes>

--- internal/pkg/discord/cmds/event/event.go
+++ internal/pkg/discord/cmds/event/event.go
@@ -102,17 +102,15 @@ func (c Command) Handle(s *discordgo.Session, i *discordgo.InteractionCreate) {
 				Data: &discordgo.InteractionResponseData{
 					Flags: 1 << 6,
 					Content: fmt.Sprintf("Created the new event! Use the below text to setup the sticky message: \n```\n %v \n```",
-						fmt.Sprintf(`
-						-sm -d 600s Want to be notified for the next public chat?
-						[https://discord.com/events/%s/%s](Click here) to get notified!
+						fmt.Sprintf(`-sm -d 600s **Want to be notified for the next public chat?**
+[https://discord.com/events/%s/%s](Click here) to get notified!
 
-						Why do some users have '#' and '##' in their name?
-						Please see [https://discord.com/channels/472883504217063466/929655565364375602/1005430414384582737](this) message for more information!
+**Why do some users have '#' and '##' in their name?**
+Please see [https://discord.com/channels/472883504217063466/929655565364375602/1005430414384582737](this) message for more information!
 
-						How can I contact a mod? I cannot see any other channels
-						- Please DM any of the users with the <@&866451599270084639> or <@&934372109717737523> roles
-						- The only other public channel is <#866444906142892033>
-						`, i.GuildID, event.ID),
+**How can I contact a mod? I cannot see any other channels**
+- Please DM any of the users with the <@&866451599270084639> or <@&934372109717737523> roles
+- The only other public channel is <#866444906142892033>`, i.GuildID, event.ID),
 					),
 				},
 			})
